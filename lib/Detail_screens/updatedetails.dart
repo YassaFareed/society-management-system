183bfe06f4a5576b0e2272ce2368a68b6be4c1b0
YassaFareed <yassa.fareed99@gmail.com>
2020-11-09 00:21:11 +0500

    Create updatedetails.dart

---
+++ lib/Detail_screens/updatedetails.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class updateDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(vertical: 20),
+        color:Colors.blue,
+
+
+
+      ),
+    );
+  }
+}
