5f728bf60fc36187bc2a8293712f24181d498b33
YassaFareed <yassa.fareed99@gmail.com>
2020-11-09 00:21:22 +0500

    Update homescreen.dart

--- lib/home_screens/homescreen.dart
+++ lib/home_screens/homescreen.dart
@@ -15,9 +15,137 @@ class _homeScreenState extends State<homeScreen> {
         title: Text('home'),
         backgroundColor: Colors.black,
       ),
-        drawer:mainDrawer()
+        drawer:mainDrawer(),
+        body: Attendance(),
     );
 
   }
-
 }
+
+class Attendance extends StatefulWidget {
+  @override
+  _AttendanceState createState() => _AttendanceState();
+}
+
+class _AttendanceState extends State<Attendance> {
+  int absent =0;
+  int present =0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Container(
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children:[
+            Expanded(
+              flex:1,
+                child:Container(
+                  child:Text(
+                    'Attendance Status',
+                    textAlign:TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold, height:3, fontSize:30),
+                  ),
+                  height:200,
+
+                  color:Colors.black12,
+                )
+            ),
+            Expanded(
+              flex:1,
+              child: Container(
+
+                color:Colors.purpleAccent,
+                child:Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+
+                  children: [
+                    Expanded(
+                      child:Container(
+                        child:Text(
+                          'This Month:',
+                          textAlign:TextAlign.center,
+                          style: TextStyle(fontWeight: FontWeight.bold, height:3, fontSize:30),
+                        ),
+
+                        color:Colors.white,
+                        height: 100,
+                        width: 50,
+                      ),
+                    ),
+                    Expanded(
+                      flex:1,
+                      child:Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children:[
+                        Expanded(
+                         child: Container(
+                            child:Text(
+                              absent.toString(),
+                          textAlign:TextAlign.center,
+                          style: TextStyle(fontWeight: FontWeight.bold, height:1.5, fontSize:25),
+                        ),
+                        color:Colors.redAccent,
+                        height: 50,
+                        width: 100,
+                      ),
+                        ),
+                        Expanded(
+                          flex:1,
+                          child:Container(
+                            child:Text(
+                              present.toString(),
+                              textAlign:TextAlign.center,
+                              style: TextStyle(fontWeight: FontWeight.bold, height:1.5, fontSize:25),
+                            ),
+                            color:Colors.green,
+                            height: 50,
+                            width: 100,
+                          ),
+                        ),
+
+                     ],
+                    ),
+                    ),
+                  ],
+                )
+
+              ),
+            ),
+            Expanded(
+                flex:1,
+
+                child: Container(
+                     child:Text(
+                    'Mark your attendence here',
+                    textAlign:TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.bold, height:2, fontSize:25),
+                  ),
+
+
+                  color:Colors.white,
+                ),
+
+                ),
+
+
+            Expanded(
+              flex:4,
+              child:Container(
+
+                color:Colors.white,
+              )
+            ),
+
+
+                 ],
+
+            ),
+            )
+    );
+
+  }
+}
+
