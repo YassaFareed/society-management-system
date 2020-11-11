b5e03c98b281b76caeff8b905873d8da719a1153
YassaFareed <yassa.fareed99@gmail.com>
2020-11-12 00:37:25 +0500

    Create complainscreen.dart

---
+++ lib/setup_screens/complainscreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class complainManagement extends StatefulWidget {
+  @override
+  _complainManagementState createState() => _complainManagementState();
+}
+
+class _complainManagementState extends State<complainManagement> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text(''
+          'hello'
+          ),
+    );
+  }
+}
