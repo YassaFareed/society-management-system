31da28ff328526f85e02de246832752d2d39ac69
YassaFareed <yassa.fareed99@gmail.com>
2020-11-13 21:34:46 +0500

    Create payrolldetail.dart

---
+++ lib/Detail_screens/payrolldetail.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class payrollWorker extends StatefulWidget {
+  @override
+  _payrollWorkerState createState() => _payrollWorkerState();
+}
+
+class _payrollWorkerState extends State<payrollWorker> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Text('hello'),
+
+    );
+  }
+}
