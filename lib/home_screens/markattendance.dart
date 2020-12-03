0606e17cc965bc6bc517585a7b7fe1b3a890023c
YassaFareed <yassa.fareed99@gmail.com>
2020-12-03 17:59:28 +0500

    added timer

--- lib/home_screens/markattendance.dart
+++ lib/home_screens/markattendance.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -13,6 +15,49 @@ class _attendanceMarkState extends State<attendanceMark> {
   String value = 'Check-in';
   DateFormat dateFormat = DateFormat("dd-MM-yyyy  HH:mm:ss");
 
+  //stopwatch
+  bool resetispressed = true;
+  bool startispressed = true;
+  bool stopispressed = true;
+  String stoptimetodisplay = "00:00:00";
+  var swatch = Stopwatch();
+  final dur = const Duration(seconds: 1);
+
+  void starttimer(){
+    Timer(dur, keeprunning); // starts a timer
+  }
+
+  void keeprunning(){
+    if(swatch.isRunning){
+      starttimer();
+    }
+    setState(() {
+      stoptimetodisplay = swatch.elapsed.inHours.toString().padLeft(2, "0") + ":" //if string is < width 2 then add 0 on left
+          + (swatch.elapsed.inMinutes%60).toString().padLeft(2, "0") + ":"
+          + (swatch.elapsed.inSeconds%60).toString().padLeft(2, "0");
+    });
+  }
+
+  /*void resetstopwatch(){
+    setState(() {
+      startispressed = true;
+      resetispressed = true;
+    });
+    swatch.stop();
+   // stoptimetodisplay = "00:00:00";
+  }*/
+  void stopstopwatch(){
+    setState(() {
+      stopispressed = true;
+      resetispressed = false;
+    });
+    swatch.stop();
+  }
+
+  void startstopwatch(){
+    swatch.start();
+    starttimer();
+  }
 
   void isTest()
   {
@@ -20,50 +65,76 @@ class _attendanceMarkState extends State<attendanceMark> {
     if(pressAttention==false)
     {
       value='Check-in';
+      stopstopwatch();
+
     }
     else{
       value='Check-out';
+      startstopwatch();
+
     }
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
       body:Center(
-         child:Column(
+        child:Column(
           mainAxisAlignment: MainAxisAlignment.center,
-           children:[
+          children:[
 
-             Text('Current Time',
-                 style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold)),
-             SizedBox(
-               height: 10,
-             ),
-             Text(dateFormat.format(DateTime.now()).toString(),
-                 style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold)),
-             SizedBox(
-               height: 60,
-             ),
-             Container(
-               width: 200,
-               height: 200,
-               child: MaterialButton(
+            Text('Current Time',
+                style: TextStyle(fontSize: 30,fontWeight: FontWeight.w700)),
+            SizedBox(
+              height: 10,
+            ),
+            Text(dateFormat.format(DateTime.now()).toString(),
+                style: TextStyle(fontSize: 30,fontWeight: FontWeight.w700)),
+            SizedBox(
+              height: 60,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              child: MaterialButton(
+
                 child: Text(value, style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-               textColor: Colors.white,
-               shape: CircleBorder(
-                   side: BorderSide(width: 6, color: Colors.red, style: BorderStyle.solid)
-               ),
-               color: pressAttention ? Colors.orange : Colors.blue,
+                textColor: Colors.white,
+                shape: CircleBorder(
+                    side: BorderSide(width: 6, color: Colors.red, style: BorderStyle.solid)
+                ),
+                color: pressAttention ? Colors.orange : Colors.blue,
 
 
-               onPressed: () => setState(() {
-                 isTest();
-               }),
-             ),
-             ),
-           ],
+                onPressed: () => setState(() {
+                  isTest();
+                }),
+              ),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            Text(
+              'Hours Worked',
+              style: TextStyle(
+                fontSize:30.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Container(
+              alignment:Alignment.center,
+              child: Text(
+                stoptimetodisplay,
+                style: TextStyle(
+                  fontSize:30.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+
+            ),
+          ],
 
 
-         ),
+        ),
       ),
     );
   }
