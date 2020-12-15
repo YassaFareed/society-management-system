ee85d5cefdb96086b6ceed3ad7e175575dc38230
YassaFareed <yassa.fareed99@gmail.com>
2020-12-15 23:39:27 +0500

    Update payrolldetail.dart

--- lib/Detail_screens/payrolldetail.dart
+++ lib/Detail_screens/payrolldetail.dart
@@ -1,21 +1,34 @@
-import 'package:fl_chart/fl_chart.dart';
+
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:societyworker/services/workerpayroll.dart';
+import 'package:societyworker/widgets/Columnwidget.dart';
 
 class payrollWorker extends StatefulWidget {
   @override
   _payrollWorkerState createState() => _payrollWorkerState();
 }
 
+List payroll;
 class _payrollWorkerState extends State<payrollWorker> {
 
-  List<PieChartSectionData> _sections = List<PieChartSectionData>();
-  int absent =5;
-  int present =20;
+
+
   @override
 
   void initState() {
     super.initState();
+    PayrollService().getWorkerPayroll();
+    print("HELO THIS IS INIT PAYROLL");
+    print(payroll);
+
+/*
+
+    List<PieChartSectionData> _sections = List<PieChartSectionData>();
+    int absent =5;
+    int present =20;
+
+
 
     PieChartSectionData _item1 = PieChartSectionData(
       color: Colors.green,
@@ -32,207 +45,34 @@ class _payrollWorkerState extends State<payrollWorker> {
       radius: 50,
       titlePositionPercentageOffset: 0.5,
       titleStyle:TextStyle(color: Colors.white,fontSize:20),
-    );
 
-    _sections = [_item1,_item2];
+    );
+    _sections=[_item1,_item2]*/
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:ListView(
-        padding: const EdgeInsets.all(20),
-        children:[
-         Card(
-           elevation: 10,
-           shape: RoundedRectangleBorder(
-             borderRadius: BorderRadius.circular(15.0),
-           ),
-              child:Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
+    return  Scaffold(
+      body: ListView.builder(
+          itemCount: payroll.length==0 ?1: payroll.length,
+          itemBuilder: (context , index){
+            return ColoumnWidget(totalPresent:payroll[index]['totalPresent'],totalAbsent:payroll[index]['totalAbsent'],name:payroll[index]['name'],basicSalary:payroll[index]['basicSalary'],workerId: payroll[index]['workerId'],bonus: payroll[index]['bonus'],deductionsPerAbsent:payroll[index]['deductionsPerAbsent'],totalDeductions: payroll[index]['totalDeductions'], totalIncome:payroll[index]['totalIncome'],objId: payroll[index]['_id'],);
+          }
 
-                  width: MediaQuery.of(context).size.width*0.9,
-                  child: Row(
-                   children:[
-                    Ink(
-
-                      child: Icon(
-                       Icons.layers,
-                        size:30.0,
-                       ),
-                        ),
-                     SizedBox(width: MediaQuery.of(context).size.width*0.12),
-                    Text('Total Present',style: TextStyle( fontSize:22.0)),
-                     SizedBox(width: MediaQuery.of(context).size.width*0.05),
-                    Text('20',style: TextStyle( fontSize:25.0)),
-                  ],
-            ),
-                ),
-              ),
-
-
-         ),
-        SizedBox(height:10,),
-          Card(
-            elevation: 10,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child:Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-
-                width: MediaQuery.of(context).size.width*0.9,
-                child: Row(
-                  children:[
-                    Ink(
-
-                      child: Icon(
-                        Icons.layers_clear,
-                        size:30.0,
-                      ),
-                    ),
-                    SizedBox(width: MediaQuery.of(context).size.width*0.135),
-                    Text('Total Absent',style: TextStyle( fontSize:22.0)),
-                    SizedBox(width: MediaQuery.of(context).size.width*0.05),
-                    Text('5',style: TextStyle( fontSize:25.0)),
-                  ],
-                ),
-              ),
-            ),
-
-          ),
-          SizedBox(height:20,),
-
-          Container(
-            height: 280,
-            color:Colors.white,
-            child: Card(
-              elevation: 20,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0)),
-              child:AspectRatio(
-                aspectRatio: 1,
-                child: FlChart(
-                  chart: PieChart(
-                    PieChartData(sections: _sections, borderData:FlBorderData(show:false), centerSpaceRadius: 80, sectionsSpace:6),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 10,),
-          Card(
-            elevation: 10.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-
-            child: Padding(
-              padding: const EdgeInsets.all(3.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Center(
-                    child: Padding(
-                      padding: const EdgeInsets.only(top:8.0),
-                      child: AutoSizeText(
-                        'Income Summary' ,
-                        style: TextStyle(fontSize: 16.0,fontWeight: FontWeight.bold),
-                        maxLines: 2,
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(height: 8,),
-
-                  Padding(
-                    padding: const EdgeInsets.only(left:10.0,bottom: 3),
-                    child: RichText(
-                      text: TextSpan(style: new TextStyle(fontSize: 14.0, color: Colors.black,),
-                        children: <TextSpan>[
-                          new TextSpan(text: 'Name: ',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold)),
-                          new TextSpan(text: 'Ali', style: new TextStyle(fontSize: 15),)],
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 8,),
-
-                  Padding(
-                    padding: const EdgeInsets.only(left:10.0,bottom: 3),
-                    child: RichText(
-                      text: TextSpan(style: new TextStyle(fontSize: 14.0, color: Colors.black,),
-                        children: <TextSpan>[
-                          new TextSpan(text: 'Basic Salary: ',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold)),
-                          new TextSpan(text: '\$5000', style: new TextStyle(fontSize: 15),)],
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(height: 8,),
-                  Padding(
-                    padding: const EdgeInsets.only(left:10.0,bottom: 3),
-                    child: RichText(
-                      text: TextSpan(style: new TextStyle(fontSize: 14.0, color: Colors.black,),
-                        children: <TextSpan>[
-                          new TextSpan(text: 'Bonus:',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold)),
-                          new TextSpan(text: '\$1000', style: new TextStyle(fontSize: 15),)],
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(height:8),
-
-                  Padding(
-                    padding: const EdgeInsets.only(left:10.0,bottom: 3),
-                    child: RichText(
-                      text: TextSpan(style: new TextStyle(fontSize: 14.0, color: Colors.black,),
-                        children: <TextSpan>[
-                          new TextSpan(text: 'Deduction per absence: ',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold)),
-                          new TextSpan(text: '\$200', style: new TextStyle(fontSize: 15),)],
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(height:8),
-                  Padding(
-                    padding: const EdgeInsets.only(left:10.0,bottom: 3),
-                    child: RichText(
-                      text: TextSpan(style: new TextStyle(fontSize: 14.0, color: Colors.black,),
-                        children: <TextSpan>[
-                          new TextSpan(text: 'Total Deduction :',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold)),
-                          new TextSpan(text: '\$1000', style: new TextStyle(fontSize: 15),)],
-                      ),
-                    ),
-                  ),
-
-                  SizedBox(height: 8,),
-
-                  Padding(
-                    padding: const EdgeInsets.only(left:10.0,bottom: 3),
-                    child: RichText(
-                      text: TextSpan(style: new TextStyle(fontSize: 14.0, color: Colors.black,),
-                        children: <TextSpan>[
-                          new TextSpan(text: 'Total Income :',style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold)),
-                          new TextSpan(text: '\$5000', style: new TextStyle(fontSize: 15),)],
-
-                            ),
-                    ),
-                  ),
-                  SizedBox(height: 8,),
-
-                ],
-              ),
-            ),
-          ),
-
-
-        ],
       ),
 
-
     );
+   // payroll['totalPresent']
+   // return ColoumnWidget(totalPresent:500,totalAbsent:2,name:"hassan",basicSalary:500,workerId:"213123",bonus: 500,deductionsPerAbsent:2222,totalDeductions: 2200, totalIncome:500,objId:"2313",);
+ /* return Scaffold(
+    body:Container(
+        child:Text(
+            payroll[0]['totalPresent']
+        ),
+    ),
+  );
+
+*/
+
   }
 }
